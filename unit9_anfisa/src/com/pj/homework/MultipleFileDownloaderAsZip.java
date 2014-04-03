360fc5c349a9423eb415e5a4d66a5babeace5281
Sergey Travin <sergey.a.travin@gmail.com>
2014-04-03 23:16:09 +0600

    Reformat code

--- unit9_anfisa/src/com/pj/homework/MultipleFileDownloaderAsZip.java
+++ unit9_anfisa/src/com/pj/homework/MultipleFileDownloaderAsZip.java
@@ -11,54 +11,54 @@ import java.util.zip.ZipOutputStream;
 
 /**
  * 
- * Multiple files downloader as one archive
- * list URLs to files as command line arguments
- *
+ * Multiple files downloader as one archive list URLs to files as command line
+ * arguments
+ * 
  */
 
-public class MultipleFileDownloaderAsZip  {
+public class MultipleFileDownloaderAsZip {
 
-  public static void main(String[] args) throws IOException  {
+	public static void main(String[] args) throws IOException {
 
-    FileOutputStream fout = new FileOutputStream (new File("myArchive.zip")); 
-    ZipOutputStream zout = new ZipOutputStream(fout);
-    
-    for (String filename : args) {
-     
-      URL remoteFile=new URL(filename);      
-      URLConnection urlConnFileStream=remoteFile.openConnection();
-      String[] tokens = filename.split("/"); // get file name from URL
-      InputStream in=urlConnFileStream.getInputStream();
-      ZipEntry ze = new ZipEntry(tokens[tokens.length - 1]);
-      System.out.println("Downloading " + tokens[tokens.length - 1]); 
-      
-      try{
-        zout.putNextEntry(ze);
-        double i=0;
-        double j=0;
-        for (int c = in.read(); c != -1 ; c = in.read()) {
-            zout.write(c);
-            i++;
-            if ( (i % 100000) == 0 ) {
-              j++;
-              if (j==63) {
-                System.out.println(".");
-                j=0;
-              }
-              System.out.print(".");
-            }
-        }
-        
-        System.out.println("\nFinished download of " + filename);
-       
-      } catch (Exception e) { 
-        e.printStackTrace();
-      }  finally {
-        in.close();
-      } 
-      
-    }
-    System.out.println("Done.");
-    zout.close();
-  }
+		FileOutputStream fout = new FileOutputStream(new File("myArchive.zip"));
+		ZipOutputStream zout = new ZipOutputStream(fout);
+
+		for (String filename : args) {
+
+			URL remoteFile = new URL(filename);
+			URLConnection urlConnFileStream = remoteFile.openConnection();
+			String[] tokens = filename.split("/"); // get file name from URL
+			InputStream in = urlConnFileStream.getInputStream();
+			ZipEntry ze = new ZipEntry(tokens[tokens.length - 1]);
+			System.out.println("Downloading " + tokens[tokens.length - 1]);
+
+			try {
+				zout.putNextEntry(ze);
+				double i = 0;
+				double j = 0;
+				for (int c = in.read(); c != -1; c = in.read()) {
+					zout.write(c);
+					i++;
+					if ((i % 100000) == 0) {
+						j++;
+						if (j == 63) {
+							System.out.println(".");
+							j = 0;
+						}
+						System.out.print(".");
+					}
+				}
+
+				System.out.println("\nFinished download of " + filename);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				in.close();
+			}
+
+		}
+		System.out.println("Done.");
+		zout.close();
+	}
 }
